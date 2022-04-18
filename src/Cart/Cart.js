8eae8328c10e31492cf145bcd0237eeb658f86d3
ajoy biswas <ajoypust@gmail.comm>
2022-04-18 13:22:30 +0600

    randon choice fron cart

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Cart.css'
 const Cart = ({cart,products,removeCartProduct}) => {
-    // console.log(cart)
+     console.log(cart)
     // console.log(products)
-       
+    const [offer,setOffer]= useState([]);
+    const offerButton=()=>{
+        const randomNumber=Math.floor( Math.random()*cart.length);
+        const item=cart[randomNumber];
+      //   console.log(item)
+      //   console.log(randomNumber)
+      setOffer(item);
+    }
        const product= cart.map(product=>product )
   
     return (
@@ -19,6 +26,14 @@ const Cart = ({cart,products,removeCartProduct}) => {
              }
            
            <button className='remove-btn' onClick={()=>removeCartProduct(product.id)}>Remove Select Product</button>
+
+           <button onClick={offerButton} className='remove-btn'>Choose one From cart</button>
+           {
+             Object.keys(offer).length>0 &&<div className='offer'>
+                 <img width={50} height={50} src={offer.img} alt="" />
+                <h1>{offer.name}</h1>
+                </div>
+          } 
         </div>
     );
 };
